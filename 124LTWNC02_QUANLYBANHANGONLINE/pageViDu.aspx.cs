6ef9b7c8dbe9d33eb2f0fc3ca5c280d87b82c2ba
camapcon0702 <quyle070204@gmail.com>
2024-10-23 09:05:30 +0700

    comit

---
+++ 124LTWNC02_QUANLYBANHANGONLINE/pageViDu.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _124LTWNC02_QUANLYBANHANGONLINE
+{
+    public partial class pageViDu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dbQUANLYBANHANGEntities db = new dbQUANLYBANHANGEntities();
+            var tbsanpham = from tbSANPHAM in db.tbSANPHAM select tbSANPHAM;
+
+            this.GridView1.DataSource = tbsanpham.ToList<tbSANPHAM>();
+            this.GridView1.DataBind();
+        }
+    }
+}
